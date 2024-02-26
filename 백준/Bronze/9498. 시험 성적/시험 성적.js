16b0a4df57dfe751c6bbce8ef697708c96c3e3dd
Soohyun Kim <122338050+gamjatan9@users.noreply.github.com>
2024-02-26 11:41:09 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,11 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const mark = Number(input[0]);
+function grade(a){
+  if(a <= 100 && a >= 90) console.log('A');
+  else if(a <= 89 && a >= 80) console.log('B');
+  else if(a <= 79 && a >= 70) console.log('C');
+  else if(a <= 69 && a >= 60) console.log('D');
+  else console.log('F')
+}
+
+grade(mark);
